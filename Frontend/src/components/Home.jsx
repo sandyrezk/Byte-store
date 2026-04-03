5e7d8fa0dcacde0648839616f88157a57fc27b60
sandyrezk <sandyrezk005@gmail.com>
2026-04-04 01:36:00 +0200

    Add files via upload

---
+++ Frontend/src/components/Home.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Productlistitems from './products/productlistitems'
+import Categories from './categories/Categories'
+import { useParams } from 'react-router-dom'
+
+export default function Home() {
+  const [products, setProducts] = useState([])
+  const{category_id}=useParams()
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        if(category_id){
+          const response = await axios.get(`http://localhost:3001/products/category/${category_id}`)
+        setProducts(response.data)
+        }else{
+          const response = await axios.get('http://localhost:3001/products')
+        setProducts(response.data)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchProducts()
+  }, [category_id])
+
+  return (
+    <div className='container'>
+      <Categories/>
+      <div className='row my-5'>
+        {products?.map(product => (
+          <Productlistitems key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  )
+}
